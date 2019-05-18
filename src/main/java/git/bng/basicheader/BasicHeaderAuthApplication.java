ce075b0ac30a259cda139cf6c3ccc561241d42c0
b-ng <b-ng.git@pm.me>
2019-05-17 20:46:24 -0700

    updated name

--- src/main/java/git/bng/basicheader/BasicHeaderAuthApplication.java
+++ src/main/java/git/bng/basicheader/BasicHeaderAuthApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicheaderApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BasicheaderApplication.class, args);
-	}
-
+public class BasicHeaderAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BasicHeaderAuthApplication.class, args);
+    }
 }
